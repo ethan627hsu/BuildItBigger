1413dc94a99457d72a37ff4cb909e8a9abd0c773
Ethan Hsu <ethan627hsu@gmail.com>
2018-07-05 15:02:19 -0700

    Added changed files

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeQueryTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeQueryTest.java
@@ -1,11 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.udacity.gradle.builditbigger.MainActivity;
-import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.builditbigger.paid.MainActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivity.java
@@ -0,0 +1,110 @@
+package com.udacity.gradle.builditbigger.paid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+import com.udacity.gradle.builditbigger.jokedisplaylib.JokeDisplay;
+import com.udacity.gradle.jokeLib.JokeData;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    onJokeQuery reponseTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+
+        new EndpointsAsyncTask().execute(this);
+
+    }
+
+    public void setJokeResponse(onJokeQuery callback) {
+        reponseTask = callback;
+    }
+
+    public interface onJokeQuery {
+        void jokeQueried(String response);
+    }
+
+    class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Context... context) {
+            if (myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+
+                myApiService = builder.build();
+            }
+
+            context = context;
+
+            try {
+                return myApiService.getJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //        Toast.makeText(this, JokeData.getRandomJoke(), Toast.LENGTH_SHORT).show();
+            reponseTask.jokeQueried(result);
+            Intent launchIntent = new Intent(MainActivity.this, JokeDisplay.class);
+            launchIntent.putExtra(JokeDisplay.EXTRA_JOKE_DATA, JokeData.getRandomJoke());
+            startActivity(launchIntent);
+        }
+    }
+
+
+}
